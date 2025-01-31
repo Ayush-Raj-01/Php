2457370d079d0aadf2b26e0b47c1b0ff9d4aec17
Ayush Raj <ayussshhraj@gmail.com>
2025-01-31 13:42:27 +0530

    operators

---
+++ 06_operators.php
@@ -0,0 +1,26 @@
+<?php
+
+$x = 10;
+$y = 20;
+
+echo "x + y: " . ($x + $y) . "\n";
+echo "x - y: " . ($x - $y) . "\n";
+echo "x / y: " . ($x / $y) . "\n";
+echo "x * y: " . ($x * $y) . "\n";
+
+// increment and decrement operator 
+
+echo "value of X before incrementation : $x \n";
+echo "x++: " . $x++ . "\n";
+echo "x: " . $x . "\n";
+
+echo "++x: " . ++$x . "\n";
+echo "x: " . $x . "\n";
+
+echo "value of y before decrementation : $y \n";
+echo "y--: " . $y-- . "\n";
+echo "y: " . $y . "\n";
+
+echo "--y: " . --$y . "\n";
+echo "y: " . $y . "\n";
+?>
