c58f0a40ad063285c79bf64f414010110816439b
Ayush Raj <ayussshhraj@gmail.com>
2025-01-31 13:41:21 +0530

    concat in php

---
+++ 05_concat.php
@@ -0,0 +1,12 @@
+<?php
+$firstname = "Ayush";
+$lastname = "Raj";
+
+echo $firstname . "\n";
+echo $lastname . "\n";
+
+// concatenate the string with a space between 
+
+$fullname = $firstname . " " . $lastname; 
+echo "Fullname: " . $fullname . "\n";
+?>
