7ae4cdcd65520120042699de451d1d8989a5236b
Ayush Raj <ayussshhraj@gmail.com>
2025-02-02 23:02:48 +0530

    Nested Switch

---
+++ 08_nestedSwitch.php
@@ -0,0 +1,114 @@
+<?php
+
+// $brand = "BMW";
+// $model = "M5 Cs";
+// switch ($brand) {
+//     case 'Toyota':
+//         switch ($model) {
+//             case 'Supra':
+//                 echo "Toyota Supra, the price is $7000";
+//                 break;
+//             case "Fortuner":
+//                 echo "Toyota Fortuner, the price is 40,000 Rs";
+//                 break;;
+//             default:
+//                 echo "Brand found but model not available";
+//         }
+//         break;
+
+//         case "BMW":
+//             switch ($model){
+//                 case "M5 Cs":
+//                     echo "BMW MS Cs the price is 5000$";
+//                     break;
+//                 case 'M4':
+//                     echo "BMW M4 the price is ------";
+//                     break;
+//                 default:
+//                 echo "brand found model not ----";
+//             }
+//             break;
+    
+//     default:
+//         echo "Brand not found";
+//         break;
+// }
+
+
+
+
+// $day = "Saturday";
+// $time = "Evening";
+
+// switch ($day) {
+//     case 'Weekday':
+//         switch ($time) {
+//             case 'Matinee':
+//                 echo "Weekday matinee the price is 84";
+//                 break;
+//             case "Evening":
+//                 echo "weekday evening, the price is 34";
+//                 break;
+//             default:
+//                 echo "invalid time slot";
+//         }
+//         break;
+//     case 'Saturday':
+//     case "Sunday";
+//     switch ($time) {
+//         case 'Matinee':
+//             echo "Weekend matinee the price is 840";
+//             break;
+//         case "Evening":
+//             echo "weekend evening, the price is 300";
+//             break;
+//         default:
+//             echo "invalid time slot";
+//     }
+//     break;
+//     default:
+//         echo "invalid date and time";
+//         break;
+// }
+
+
+
+
+
+
+// $catogery = "main course";
+// $dish = "chicken";
+
+// switch ($catogery) {
+//     case 'Starter':
+//         switch ($dish) {
+//             case 'soup':
+//                 echo "soup the price is 84";
+//                 break;
+//             case "salad":
+//                 echo "salad, the price is 99";
+//                 break;
+//             default:
+//                 echo "dish not available in starter";
+//         }
+//         break;
+
+//         case 'main course':
+//             switch ($dish) {
+//                 case 'chicken':
+//                     echo "chicken the price is 840";
+//                     break;
+//                 case "pasta":
+//                     echo "salad, the price is 349";
+//                     break;
+//                 default:
+//                     echo "dish not available in main course";
+//             }
+//             break;
+
+//     }
+
+    
+    // write a php pr that display the listof the price of the room in a hotel on the basis of the type and the season 
+    // wap that display the price of the ticket ba
+    
